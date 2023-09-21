37696198aab4c6ea96b9a2d80a697a269beb283c
jst267 <64456259+jst267@users.noreply.github.com>
2023-09-21 17:27:10 +0900

    check

--- front/src/component/Grid/CheckBoxForGrid.tsx
+++ front/src/component/Grid/CheckBoxForGrid.tsx
@@ -1,22 +1,26 @@
-import React,{useState} from 'react';
-import { Checkbox } from 'antd';
-import type { CheckboxChangeEvent } from 'antd/es/checkbox';
+import React, { useState } from "react";
+import { Checkbox } from "antd";
+import type { CheckboxChangeEvent } from "antd/es/checkbox";
 
-function CheckBoxForGrid (props) {
+function CheckBoxForGrid(props) {
   // const cellValue = props.valueFormatted ? props.valueFormatted : props.value;
 
   const onChange = (e: CheckboxChangeEvent) => {
-
     console.log(props);
     // props.setallFlage(`${e.target.checked}`);
+
+    if (e.target.checked) {
+      props.setValue("Y");
+    } else {
+    }
     props.setValue(`${e.target.checked}`);
   };
 
   return (
     <span>
-      <Checkbox onChange={onChange}></Checkbox>
+      <Checkbox onChange={onChange} checked={props.value == "Y"}></Checkbox>
     </span>
   );
-};
+}
 
 export default CheckBoxForGrid;
