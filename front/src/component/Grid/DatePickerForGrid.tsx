199c7002ebc18d70890d9a0c41f5b4daa13bb6ae
jwpark2023 <139726025+jwpark2023@users.noreply.github.com>
2023-09-21 16:26:26 +0900

    dd

--- front/src/component/Grid/DatePickerForGrid.tsx
+++ front/src/component/Grid/DatePickerForGrid.tsx
@@ -1,25 +1,45 @@
-import { DatePicker, DatePickerProps } from 'antd';
-import dayjs from 'dayjs';
-import {useEffect} from "react";
+import { DatePicker, DatePickerProps } from "antd";
+import dayjs from "dayjs";
+import { useEffect } from "react";
 
-function DatePickerForGrid (props) {
+function DatePickerForGrid(props) {
+  const { RangePicker } = DatePicker;
+  const onChange = (value, dateString) => {
+    if (props.range)
+      props.setValue([
+        value[0].format("YYYY-MM-DD"),
+        value[1].format("YYYY-MM-DD"),
+      ]);
+    else props.setValue(value.format("YYYY-MM-DD"));
+  };
 
- const { RangePicker } = DatePicker;
- const onChange = (value, dateString) => {
-
-   if(props.range)
-      props.setValue([value[0].format("YYYY-MM-DD"),value[1].format("YYYY-MM-DD")]);
-   else
-      props.setValue(value.format("YYYY-MM-DD"));
-
- }
-
-
- if(props.range)
-    return <span><RangePicker defaultValue={props.defaultValue} value={[dayjs(props.value[0], "YYYY-MM-DD"), dayjs(props.value[1], "YYYY-MM-DD")]} format={props.format} onChange={onChange}/></span>
- else
-    return  <span><DatePicker defaultValue={props.defaultValue} value={dayjs(props.value, "YYYY-MM-DD")} format={props.format} onChange={onChange}/></span>
-
+  if (props.range)
+    return (
+      <span>
+        <RangePicker
+          value={
+            props.value
+              ? [
+                  dayjs(props.value[0], "YYYY-MM-DD"),
+                  dayjs(props.value[1], "YYYY-MM-DD"),
+                ]
+              : undefined
+          }
+          format={props.format}
+          onChange={onChange}
+        />
+      </span>
+    );
+  else
+    return (
+      <span>
+        <DatePicker
+          value={props.value ? dayjs(props.value, "YYYY-MM-DD") : undefined}
+          format={props.format}
+          onChange={onChange}
+        />
+      </span>
+    );
 }
 
 export default DatePickerForGrid;
