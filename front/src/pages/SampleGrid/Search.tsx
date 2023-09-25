bf88384dc30c777244ebdb8d9294cea7450bdcb2
jst267 <64456259+jst267@users.noreply.github.com>
2023-09-25 17:16:03 +0900

    수정

--- front/src/pages/SampleGrid/Search.tsx
+++ front/src/pages/SampleGrid/Search.tsx
@@ -5,7 +5,7 @@ import dayjs from "dayjs";
 const dateFormat = "YYYY-MM-DD";
 
 const Search = forwardRef<any, any>((props, ref) => {
-  const { regGrid } = props;
+  const { refGrid } = props;
   const [form] = Form.useForm();
   const { Option } = Select;
 
@@ -50,7 +50,7 @@ const Search = forwardRef<any, any>((props, ref) => {
             fields.PERIOD[1].format(dateFormat),
           ],
         };
-        regGrid.current.getCodelist(params);
+        refGrid.current.getCodelist(params);
       })
       .catch((e) => {
         console.log("validateFields: ", e);
